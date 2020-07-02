62136775f5a724917bdca95ec9d276392fcb216a
Saito <saito_nabetani@keio.jp>
2020-07-02 19:10:20 +0900

    7章まで

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,4 +1,28 @@
 class GymsController < ApplicationController
-  def new
+
+  def show
+   @gym = Gym.find(params[:id])
   end
+
+  def new
+    @gym = Gym.new
+  end
+
+  def create
+    @gym = Gym.new(gym_params)
+    if @gym.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @gym
+     # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+     def gym_params
+       params.require(:gym).permit(:name, :address, :password, :password_confirmation)
+     end
+
 end
--- db/migrate/20200625012017_create_gyms.rb
+++ db/migrate/20200625012017_create_gyms.rb
@@ -3,7 +3,7 @@ class CreateGyms < ActiveRecord::Migration[5.1]
     create_table :gyms do |t|
       t.string :name
       t.string :address
-      # t.string :password_digest
+      t.string :password_digest
 
       t.timestamps
     end
